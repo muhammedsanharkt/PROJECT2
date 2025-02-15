18c94a7e0dd06c0c7de22c911dbf3ea98e33c464
MUHAMMED SANHAR KT <muhammedsanharkt@gmail.com>
2025-02-15 15:16:40 +0530

    'done'

---
+++ catering/js/main.js
@@ -0,0 +1,138 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner(0);
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+    
+    
+   // Back to top button
+   $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+        $('.back-to-top').fadeIn('slow');
+    } else {
+        $('.back-to-top').fadeOut('slow');
+    }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Modal Video
+    $(document).ready(function () {
+        var $videoSrc;
+        $('.btn-play').click(function () {
+            $videoSrc = $(this).data("src");
+        });
+        console.log($videoSrc);
+
+        $('#videoModal').on('shown.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+        })
+
+        $('#videoModal').on('hide.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc);
+        })
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Testimonial carousel
+    $(".testimonial-carousel-1").owlCarousel({
+        loop: true,
+        dots: false,
+        margin: 25,
+        autoplay: true,
+        slideTransition: 'linear',
+        autoplayTimeout: 0,
+        autoplaySpeed: 10000,
+        autoplayHoverPause: false,
+        responsive: {
+            0:{
+                items:1
+            },
+            575:{
+                items:1
+            },
+            767:{
+                items:2
+            },
+            991:{
+                items:3
+            }
+        }
+    });
+
+    $(".testimonial-carousel-2").owlCarousel({
+        loop: true,
+        dots: false,
+        rtl: true,
+        margin: 25,
+        autoplay: true,
+        slideTransition: 'linear',
+        autoplayTimeout: 0,
+        autoplaySpeed: 10000,
+        autoplayHoverPause: false,
+        responsive: {
+            0:{
+                items:1
+            },
+            575:{
+                items:1
+            },
+            767:{
+                items:2
+            },
+            991:{
+                items:3
+            }
+        }
+    });
+
+    // Modal Video
+$(document).ready(function () {
+    var $videoSrc;
+    
+    // Capture video source on button click
+    $('.btn-play').click(function () {
+        $videoSrc = $(this).data("src");
+    });
+
+    // When the modal is shown, set the iframe's src with autoplay
+    $('#videoModal').on('shown.bs.modal', function () {
+        $("#video").attr('src', $videoSrc + "?autoplay=1&modestbranding=1&showinfo=0");
+    });
+
+    // When the modal is hidden, stop the video by clearing the iframe's src
+    $('#videoModal').on('hide.bs.modal', function () {
+        // Clear the src to stop the video
+        $("#video").attr('src', '');
+    });
+
+    // Optional: Another way to stop the video when the modal is closed
+    $('#videoModal').on('hidden.bs.modal', function () {
+        // Reset video source after modal is fully hidden
+        $("#video").attr('src', '');
+    });
+});
+
+})(jQuery);
+
